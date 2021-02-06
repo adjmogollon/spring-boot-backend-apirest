46d4924179bffbdc0fbbc0adae0ee21e5505e402
Anibal Mogollon <adjmogollon@gmail.com>
2021-02-06 12:52:05 -0400

    S1507. Añadiendo la clase SpringSecurityConfig y registrando UserDetailsService JPA

--- src/main/java/com/adjmogollon/backend/apirest/models/dao/IUserDao.java
+++ src/main/java/com/adjmogollon/backend/apirest/models/dao/IUserDao.java
@@ -2,15 +2,11 @@ package com.adjmogollon.backend.apirest.models.dao;
 
 import com.adjmogollon.backend.apirest.models.entity.User;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IUserDao extends CrudRepository<User, Long> {
 
     public User findByUsername(String username);
-
-    @Query("select u from Usuario where u.username=?1")
-    public User findByUsernameQuery(String username);
 
     public User findByUsernameAndEmail(String username, String email);
 
