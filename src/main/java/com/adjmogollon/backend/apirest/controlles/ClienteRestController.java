ce413c1f18eaffff14507df6acb632d920c6a85c
Anibal Mogollon <adjmogollon@gmail.com>
2021-02-07 13:32:21 -0400

    S1519. Configurando Cors en Spring Security S1518. Añadiendo seguridad a nuestras ruta del controlador usando anotaciones @Secured

--- src/main/java/com/adjmogollon/backend/apirest/controlles/ClienteRestController.java
+++ src/main/java/com/adjmogollon/backend/apirest/controlles/ClienteRestController.java
@@ -30,6 +30,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -67,6 +68,7 @@ public class ClienteRestController {
         return clienteService.findAll(pageable);
     }
 
+    @Secured({"ROLE_ADMIN","ROLE_USER"})
     @GetMapping("/clientes/{id}")
     public ResponseEntity<?> show(@PathVariable Long id) {
         Cliente cliente = null;
@@ -85,6 +87,7 @@ public class ClienteRestController {
         return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @PostMapping("/clientes")
     public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
 
@@ -114,6 +117,7 @@ public class ClienteRestController {
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @PutMapping("/clientes/{id}")
     public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result, @PathVariable Long id) {
         Cliente currentCliente = this.clienteService.findById(id);
@@ -154,6 +158,7 @@ public class ClienteRestController {
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @DeleteMapping("/clientes/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         Map<String, Object> response = new HashMap<>();
@@ -174,6 +179,7 @@ public class ClienteRestController {
 
     }
 
+    @Secured({"ROLE_ADMIN","ROLE_USER"})
     @PostMapping("/clientes/upload")
     public ResponseEntity<?> upload(@RequestParam("archivo") MultipartFile archivo, @RequestParam("id") Long id) {
 
@@ -220,6 +226,7 @@ public class ClienteRestController {
         return new ResponseEntity<Resource>(recurso, cabecera, HttpStatus.OK);
     }
 
+    @Secured({"ROLE_ADMIN"})
     @GetMapping("/clientes/regiones")
     public List<Region> listarRegiones() {
         return clienteService.findAllRegiones();
