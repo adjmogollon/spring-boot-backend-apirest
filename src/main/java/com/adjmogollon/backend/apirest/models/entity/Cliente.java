7ca733959328c5a5c56ef2560c7665104a1cca05
Anibal Mogollon <adjmogollon@gmail.com>
2021-02-14 13:00:37 -0400

    S1703. Asociaciones ManyToOne Bidireccional - Clases Entity Factura y Cliente

--- src/main/java/com/adjmogollon/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/adjmogollon/backend/apirest/models/entity/Cliente.java
@@ -1,8 +1,11 @@
 package com.adjmogollon.backend.apirest.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -61,9 +65,16 @@ public class Cliente implements Serializable {
     @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
     private Region region;
 
-    // @PrePersist
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Factura> facturas;
+
+    @PrePersist
     public void prePersist() {
         createAt = new Date();
+    }
+
+    public Cliente() {
+        this.facturas = new ArrayList<>();
     }
 
     private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/adjmogollon/backend/apirest/models/entity/Factura.java
@@ -0,0 +1,53 @@
+package com.adjmogollon.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+@Entity
+@Table(name = "facturas")
+public class Factura implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+
+    private String descripcion;
+    private String observacion;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @PrePersist
+    public void prePersist() {
+        this.createAt = new Date();
+    }
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
