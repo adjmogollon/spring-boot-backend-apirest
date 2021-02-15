3236c9d8f88c330f56aa7f2563e61bf2d42a2396
Anibal Mogollon <adjmogollon@gmail.com>
2021-02-15 15:28:23 -0400

    S1700. Se aumento el tamaño de la paginacion

--- src/main/java/com/adjmogollon/backend/apirest/controlles/ClienteRestController.java
+++ src/main/java/com/adjmogollon/backend/apirest/controlles/ClienteRestController.java
@@ -64,7 +64,7 @@ public class ClienteRestController {
 
     @GetMapping("/clientes/page/{page}")
     public Page<Cliente> index(@PathVariable Integer page) {
-        Pageable pageable = PageRequest.of(page, 4);
+        Pageable pageable = PageRequest.of(page, 5);
         return clienteService.findAll(pageable);
     }
 
